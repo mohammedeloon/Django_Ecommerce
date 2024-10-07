feaa229bcf08cf39960c4496174d563fa967294a
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2024-10-08 00:11:02 +0300

    presenting products in a dynamic way

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 def index(request):
-    return render(request, 'index.html', {})
+    products = Product.objects.all() 
+    return render(request, 'index.html', {'products':products})
