37d00b2c93d5fae942ffb5e73a0368ae7198d6ee
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2024-10-08 22:19:31 +0300

    Created a user register feature

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Category, Product
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 from django.contrib import messages
+
 
 def index(request):
     products = Product.objects.all() 
@@ -25,6 +30,19 @@ def login_user(request):
     else:
         return render(request, 'login.html', {})
 
+def register_user(request):
+        form = SignUpForm()
+        if request.method == "POST":
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f'An account has been created for user {username}!')
+            else:
+                messages.success(request, ('ops! There was a problem registering the user. Please, try again.'))
+    
+        return render(request, 'register.html', {'form': form})
+        
 def logout_user(request):
     logout(request)
     messages.success(request, ('Logged Out Successfully'))
