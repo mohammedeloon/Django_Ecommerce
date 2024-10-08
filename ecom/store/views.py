5a9364049a4868a1fd8a201d5acf95242fcb4d84
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2024-10-08 03:38:14 +0300

    Created login and logout feature

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def index(request):
     products = Product.objects.all() 
     return render(request, 'index.html', {'products':products})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('logged In Successfully'))
+            return redirect('index')
+        else:
+            messages.success(request, ('There was an error! Try Again'))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('Logged Out Successfully'))
+    return redirect('index')
