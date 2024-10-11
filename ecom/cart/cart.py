5503fb57376d126ae5a5613d985cd20e6d6c7bc1
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2024-10-12 02:23:45 +0300

    Created the add to cart feature

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -4,7 +4,20 @@ class Cart():
         
         # get the current session key if existed
         cart = self.session.get('session_key')
+        # make the cart var available on all pages(global)
+        self.cart = cart
 
         # if the user is new, create a session_key
         if 'session_key' not in cart:
             cart = self.session['session_key'] = {} 
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        self.session.modified = True
+
+
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from store.models import Product
+from .cart import Cart
+from django.http import JsonResponse
 
 
 def cart_summary(request):
     return render(request, 'cart_summary.html', {})
 
 def add_cart(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post': 
+         # get data
+         product_id = int(request.POST.get('product_id'))
+         # look up product in the database
+         product = get_object_or_404(Product, id=product_id)
+         #save_to_session
+         cart.add(product=product)
+         # return response
+         response = JsonResponse({'Product_name: ': product.name})
+         return response
 
 def update_cart(request):
     pass
