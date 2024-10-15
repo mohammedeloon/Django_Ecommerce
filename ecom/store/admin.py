4ceca7280f97ce88f005a5dfeac8d59c7df4523f
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2024-10-16 00:37:10 +0300

    Created a user profile

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Category, Customer, Order, Product
+from .models import Category, Customer, Order, Product, Profile
+from django.contrib.auth.models import User
 
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+# mix profile info and user info 
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInLine]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
