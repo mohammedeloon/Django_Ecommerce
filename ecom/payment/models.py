f058acb3d6dd0f0c597764a28d5e8b493d5910f7
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2024-10-17 20:05:58 +0300

    Created order and order item models

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -18,3 +19,29 @@ class ShippingAddress(models.Model):
     # Do not pluralize model name 
     class Meta:
         verbose_name_plural = 'ShippingAddress'
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.CharField(max_length=250)
+    ShippingAddress = models.TextField(max_length=1000)
+    amount_paid = models.DecimalField(max_digits=7, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'order - {str(self.id)}'
+
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+    def __str__(self) -> str:
+        return f'order item - {str(self.id)}'
+
