a65474720e5908a6a83a794f7aaf6332bbd6f5ac
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2024-10-19 22:18:19 +0300

    Created billing info form, page and view

--- ecom/payment/forms.py
+++ ecom/payment/forms.py
@@ -16,4 +16,16 @@ class ShippingForm(forms.ModelForm):
         fields = ['shipping_full_name', 'shipping_email', 'shipping_address1', 'shipping_address2', 'shipping_city', 'shipping_state',
                   'shipping_zipcode','shipping_country', ]
         exculde = ['user', ]
+
+class PaymentForm(forms.Form):
+    card_name =  forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Name on Card'}), required=True)
+    card_number = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'card Number'}), required=True)
+    card_expiration_date = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Expiary Date'}), required=True)
+    card_cvv_number = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'CVV'}), required=True)
+    card_address1 = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Address 1'}), required=True)
+    card_address2 = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Address 2'}), required=True)
+    card_city = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'City'}), required=True)
+    card_state = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'State'}), required=True)
+    card_zipcode = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Zip Code'}), required=True)
+    card_country = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Country'}), required=True)
         
--- ecom/payment/urls.py
+++ ecom/payment/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('payment_success/', views.payment_success, name='payment_success'),
     path('checkout/', views.check_out, name='check_out'),
+    path('billing_info/', views.billing_info, name='billing_info'),
 ]
