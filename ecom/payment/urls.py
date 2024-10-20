62da7c55b013eaf2a17bf9fb064280585b952bcc
Mohammed Shurrab <102263730+mohammedeloon@users.noreply.github.com>
2024-10-20 18:38:59 +0300

    Order generation after payment

--- ecom/payment/urls.py
+++ ecom/payment/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('payment_success/', views.payment_success, name='payment_success'),
     path('checkout/', views.check_out, name='check_out'),
     path('billing_info/', views.billing_info, name='billing_info'),
+    path('process_order/', views.process_order, name= 'process_order')
 ]
--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
-from payment.models import ShippingAddress
+from payment.models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 from store.models import Profile
 from payment.forms import ShippingForm, PaymentForm
 from store.forms import UserInfoForm
@@ -36,6 +37,10 @@ def billing_info(request):
         cart_products = cart.get_items()
         cart_products_qty = cart.get_quantity()
         cart_total_price = cart.count_total()
+
+        #create a session with shipping info 
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
         if request.user.is_authenticated:
             # get the billing form 
             billing_form = PaymentForm()
@@ -43,6 +48,52 @@ def billing_info(request):
         else:
             billing_form = PaymentForm()
             return render(request, "payment/billing_info.html", {"cart_products":cart_products, "cart_products_qty":cart_products_qty, "cart_total_price":cart_total_price, "shipping_info":request.POST, "billing_form": billing_form})
+
+
+    else:
+        messages.error(request, 'Access Denied!')
+        return redirect('index')
+    
+def process_order(request):
+    if request.POST:
+        # gather cart info
+        cart = Cart(request)
+        cart_products = cart.get_items()
+        cart_products_qty = cart.get_quantity()
+        cart_total_price = cart.count_total()
+
+        # Get Billing Info From the Last Page
+        payment_Form = PaymentForm(request.POST or None)
+        # Get Shipping Session Data created in the billing info view
+        my_shipping = request.session.get('my_shipping')
+        #gather user info
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        my_address = "{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n".format(
+            my_shipping['shipping_address1'],
+            my_shipping['shipping_address2'],
+            my_shipping['shipping_city'],
+            my_shipping['shipping_state'],
+            my_shipping['shipping_zipcode'],
+            my_shipping['shipping_country']
+        )
+        amount_paid = cart_total_price
+        if request.user.is_authenticated:
+            # logged in
+            user = request.user
+            # create order for logged in users
+            order = Order(user=user, full_name=full_name, email=email, ShippingAddress=my_address, amount_paid=amount_paid)
+            order.save()
+        else:
+            #not logged in
+            # created order for anonymos users 
+            order = Order(full_name=full_name, email=email, ShippingAddress=my_address, amount_paid=amount_paid)
+            order.save()
+
+
+        print('succes')
+        messages.error(request, 'Order Places')
+        return redirect('index')
 
 
     else:
